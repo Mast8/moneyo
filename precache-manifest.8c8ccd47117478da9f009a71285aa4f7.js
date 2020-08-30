36890cdc01db167bc203ab81e2f8034289f4b0dd
mastelo <mirandarodriguezronald@gmail.com>
2020-08-29 21:35:11 -0400

    Updates

---
+++ precache-manifest.8c8ccd47117478da9f009a71285aa4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7125405459a5f36584422446b94e52a3",
+    "url": "/moneyo/index.html"
+  },
+  {
+    "revision": "430dc8c0a91cc58ec608",
+    "url": "/moneyo/static/css/main.fa5317a7.chunk.css"
+  },
+  {
+    "revision": "3bf11c528324877be9f1",
+    "url": "/moneyo/static/js/2.168a5997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moneyo/static/js/2.168a5997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430dc8c0a91cc58ec608",
+    "url": "/moneyo/static/js/main.7ee6786e.chunk.js"
+  },
+  {
+    "revision": "8a3cf647afb36f4b82b3",
+    "url": "/moneyo/static/js/runtime-main.7c30b92c.js"
+  }
+]);
